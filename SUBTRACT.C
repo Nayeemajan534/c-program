47a2cd4d71834de2382be4cbb15b072837411883
Nayeemajan534 <117568304+Nayeemajan534@users.noreply.github.com>
2022-11-06 15:53:37 +0530

    Add files via upload
    c program for subtraction.

---
+++ SUBTRACT.C
@@ -0,0 +1,14 @@
+//Program to subraction of two numbers
+#include<stdio.h>
+int main()
+{
+	int num1,num2,diff;
+	clrscr();
+	printf("Enter two numbers:\n");
+	scanf("%d  %d",&num1,&num2);
+	diff=num1-num2;
+	printf("\nDifference is : %d",diff);
+	getch();
+	return  0;
+}
+		
